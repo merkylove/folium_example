70979fc47f8509d60edf36061dea7f85993ebc52
merkylove <merkylove@users.noreply.github.com>
2016-06-10 23:39:35 +0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -8,3 +8,5 @@ httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print 'serving at port', PORT
 httpd.serve_forever()
+
+# you can run python -m SimpleHTTPServer PORT in current folder instead of running this file
